643900f39065d9a8f1ae795a41cd97fc91a8e06c
Findeton <felrobelv@gmail.com>
2022-06-12 20:20:16 +0100

    add rust wasm lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import init, {test} from 'rust-wasm-lib';
+
+init().then(() => console.log("rust-wasm-lib initialised"))
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="button" onClick={test}>
+          Run test
+        </div>
       </header>
     </div>
   );
